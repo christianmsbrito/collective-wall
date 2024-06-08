4bac9aedb68f7debf98f6d37d1421c5a835e5068
Christian M <christianmsbrito@gmail.com>
2024-06-08 18:03:34 -0300

    adding react router

--- app/frontend/components/App.jsx
+++ app/frontend/components/App.jsx
@@ -1,9 +1,17 @@
 import React from "react";
 import Chat from "./modules/Chat";
+import { Route, Routes, BrowserRouter as Router } from 'react-router-dom';
+
 
 const App = () => {
   const props = JSON.parse(document.getElementById("app-props").innerHTML);
-  return <Chat props={props} />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Chat props={props} />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
