2935900b8183ebf308add06ac3b70ea54bc6de72
Christian M <christianmsbrito@gmail.com>
2024-09-22 21:58:47 -0300

    adding protected routes

--- app/frontend/components/App.jsx
+++ app/frontend/components/App.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import Chat from "./modules/Chat";
-import { Route, Routes, BrowserRouter as Router, Navigate } from 'react-router-dom';
+import {
+  Route,
+  Routes,
+  BrowserRouter as Router,
+  Navigate,
+} from "react-router-dom";
 import Walls from "./modules/Walls";
 import LoginForm from "./modules/Login";
 import RegisterForm from "./modules/Register";
 import { UserProvider } from "./modules/contexts/UserContext";
+import ProtectedRoute from "./modules/ProtectedRoute";
 
 const App = () => {
   const props = JSON.parse(document.getElementById("app-props").innerHTML);
@@ -12,9 +18,30 @@ const App = () => {
     <Router>
       <UserProvider>
         <Routes>
-          <Route path="/" element={<Chat props={props} />} />
-          <Route path="/walls" element={<Walls props={props} />} />
-          <Route path="/walls/:id" element={<Chat props={props} />} />
+          <Route
+            path="/"
+            element={
+              <ProtectedRoute>
+                <Chat props={props} />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/walls"
+            element={
+              <ProtectedRoute>
+                <Walls props={props} />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/walls/:id"
+            element={
+              <ProtectedRoute>
+                <Chat props={props} />
+              </ProtectedRoute>
+            }
+          />
           <Route path="/register" element={<RegisterForm props={props} />} />
           <Route path="/login" element={<LoginForm props={props} />} />
           <Route path="*" element={<Navigate to="/" />} />
---
+++ app/frontend/components/modules/ProtectedRoute.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+import { useUser } from './contexts/UserContext';
+
+const ProtectedRoute = ({ children }) => {
+  const { user } = useUser();
+
+  // If the user is not logged in, redirect to the login page
+  if (!user) {
+    return <Navigate to="/login" />;
+  }
+
+  // If the user is logged in, allow access to the requested page
+  return children;
+};
+
+export default ProtectedRoute;
